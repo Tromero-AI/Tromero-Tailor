18b1031184dc07d4f339062b0055126f1d4545ad
tadhga <tadhg.amin@tromero.ai>
2024-06-15 12:19:32 +0100

    add json and streaming

--- tromero_tailor/wrapper.py
+++ tromero_tailor/wrapper.py
@@ -6,7 +6,7 @@ from openai.resources.chat.completions import (
 )
 from openai._compat import cached_property
 import datetime
-from .tromero_requests import post_data, tromero_model_create, get_model_url
+from .tromero_requests import post_data, tromero_model_create, get_model_url, tromero_model_create_stream
 from .tromero_utils import mock_openai_format
 
 
@@ -27,7 +27,8 @@ class MockCompletions(Completions):
         }
     
     def _save_data(self, data):
-        post_data(data, self._client.tromero_key)
+        if self._client.save_data:
+            post_data(data, self._client.tromero_key)
 
     def _format_kwargs(self, kwargs):
         keys_to_keep = [
@@ -35,13 +36,9 @@ class MockCompletions(Completions):
             "max_new_tokens", "ignore_eos_token", "repetition_penalty", 
             "return_full_outcome", "seed", "stop", "temperature", "top_k", 
             "top_p", "truncate", "typical_p", "watermark", "schema", 
-            "adapter_id", "adapter_source", "merged_adapters"
+            "adapter_id", "adapter_source", "merged_adapters", "response_format"
         ]
-        print("///////////////////////////")
-        print(kwargs)
         parameters = {key: kwargs[key] for key in keys_to_keep if key in kwargs}
-        print(parameters)
-        print("///////////////////////////")
         return {key: kwargs[key] for key in keys_to_keep if key in kwargs}
 
 
@@ -76,6 +73,7 @@ class MockCompletions(Completions):
         messages = kwargs['messages']
         formatted_messages =  self._format_messages(messages)
         model = kwargs['model']
+        stream = kwargs.get('stream', False)
         tags = kwargs.get('tags', [])
         formatted_kwargs = self._format_kwargs(kwargs)
         openai_kwargs = {k: v for k, v in kwargs.items() if k not in ['tags']}
@@ -95,9 +93,14 @@ class MockCompletions(Completions):
         else:
             model_name = model
             if model_name not in self._client.model_urls:
-                url = get_model_url(model_name, self._client.tromero_key)
+                url, base_model = get_model_url(model_name, self._client.tromero_key)
                 self._client.model_urls[model_name] = url
-            res = tromero_model_create(model_name, self._client.model_urls[model_name], formatted_messages, self._client.tromero_key, parameters=formatted_kwargs)
+                self._client.is_base_model[model_name] = base_model
+            model_request_name = model_name if not self._client.is_base_model[model_name] else "NO_ADAPTER"
+            if stream:
+                return tromero_model_create_stream(model_request_name, self._client.model_urls[model_name], formatted_messages, self._client.tromero_key, parameters=formatted_kwargs)
+            else:
+                res = tromero_model_create(model_request_name, self._client.model_urls[model_name], formatted_messages, self._client.tromero_key, parameters=formatted_kwargs)
             # check if res has field 'generated_text'
             if 'generated_text' in res:
                 generated_text = res['generated_text']
@@ -123,9 +126,11 @@ class MockChat(Chat):
 
 class TailorAI(OpenAI):
     chat: MockChat
-    def __init__(self, api_key, tromero_key):
+    def __init__(self, api_key, tromero_key, save_data=True):
         super().__init__(api_key=api_key)
         self.current_prompt = []
         self.model_urls = {}
+        self.is_base_model = {}
         self.tromero_key = tromero_key
         self.chat = MockChat(self)
+        self.save_data = save_data
