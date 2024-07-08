f72c66fbf317d0985931fe9c8412b3854a9d1634
Jorge Gallego Feliciano <jorge.feliciano@tromero.org>
2024-07-08 14:18:57 +0200

    fix async tests

--- tests/test.py
+++ tests/test.py
@@ -34,6 +34,22 @@ class ChatCompletion(BaseModel):
 class TestModel:
     def __init__(self, id):
         self.id = id
+        
+# Create an async mock for the list method
+class AsyncMockModels:
+    
+    def __init__(self, model_name="valid_model"):
+        self.model_name = model_name
+    
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        if not hasattr(self, '_has_returned'):
+            self._has_returned = True
+            return TestModel(self.model_name)
+        else:
+            raise StopAsyncIteration
 
 def create_chat_completion():
     return ChatCompletion(
@@ -104,7 +120,8 @@ class TestCreateChatCompletionAsync(unittest.IsolatedAsyncioTestCase):
     @patch('tromero_tailor.async_wrapper.post_data', autospec=True)
     async def test_create_with_openai_model_async(self, mock_post, mock_list_models, mock_create):
         mock_create.return_value = create_chat_completion()
-        mock_list_models.return_value = [TestModel("valid_model")]
+        mock_list_models.return_value = AsyncMockModels("valid_model")
+        
         mock_post.return_value = None
 
         result = await self.client.chat.completions.create(model="valid_model", messages=[{"content": "Hello, world!", "role": "user"}])
@@ -126,7 +143,7 @@ class TestCreateChatCompletionAsync(unittest.IsolatedAsyncioTestCase):
             system_fingerprint="example_fingerprint",
             usage=CompletionUsage(tokens=50)
         )
-        mock_list_models.return_value = [TestModel("valid_model")]
+        mock_list_models.return_value = AsyncMockModels("valid_model")
         mock_post.return_value = None
 
         result = await self.client.chat.completions.create(model="valid_model", messages=[{"content": "Hello, world!", "role": "user"}])
